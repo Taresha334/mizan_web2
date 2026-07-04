0275dd6a84e5019b78746c7d18882f9afcc44e6c
Dooflii <taresha9926@gmail.com>
2026-07-04 17:50:59 +0300

    Update build.sh with explicit PATH

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 git config --global --add safe.directory '*'
+
+# Only download if flutter folder doesn't exist
 if [ ! -d "flutter" ]; then
   curl -sL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.44.4-stable.tar.xz | tar xJ
 fi
-./flutter/bin/flutter pub get
-./flutter/bin/flutter build web --release --no-tree-shake-icons
+
+# Add the local flutter bin to the PATH for this session
+export PATH="$PATH:`pwd`/flutter/bin"
+
+# Run the commands
+flutter pub get
+flutter build web --release --no-tree-shake-icons
